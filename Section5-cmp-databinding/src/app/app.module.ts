464c13c7c1f366a0c012f37b071a3fc192e90f01
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-10-18 20:57:44 +0530

    cmp databinding init

---
+++ Section5-cmp-databinding/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CockpitComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
