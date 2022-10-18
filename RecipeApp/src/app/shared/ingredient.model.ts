b7338b0f4c0c8ace656d1eff8a51d35caac857cb
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-10-18 20:41:41 +0530

    ingredients

---
+++ RecipeApp/src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    //Typescript has a shortcut for the above as mentioned below
+    constructor(public name: string, public amount: number) {
+    }
+}
