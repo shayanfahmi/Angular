8b87fd63b53ca41394b1ea4115497b8b77a8a365
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-10-18 18:10:08 +0530

    server changes

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -1,9 +1,14 @@
+import { style } from "@angular/animations";
 import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
-    
+    templateUrl: './server.component.html',
+    styles: [`
+        .online{
+            color: white;
+        }
+    `]
 })
 export class ServerComponent {
     serverId: number = 10;
