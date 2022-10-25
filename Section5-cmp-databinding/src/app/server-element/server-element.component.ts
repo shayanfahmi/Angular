e3d1c3d33c98386814519744377acf52c7f6a2c2
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-10-25 20:18:49 +0530

    content child

--- Section5-cmp-databinding/src/app/server-element/server-element.component.ts
+++ Section5-cmp-databinding/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -10,6 +10,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   @Input('srvElement') element: { type: string, name: string, content: string };
   @Input() name: string;
   @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
   constructor() {
     console.log('constructor called.');
   }
@@ -22,12 +23,15 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   ngAfterViewInit(): void {
     console.log('ngAfterViewInit called');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    // console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
   ngAfterContentChecked(): void {
     console.log('ngAfterContentChecked called');
   }
   ngAfterContentInit(): void {
     console.log('ngAfterContentInit called')
+    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
+
   }
   ngDoCheck(): void {
     console.log('ngDoCheck called');
@@ -41,6 +45,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   ngOnInit(): void {
     console.log('ngOnInit called');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
 }
