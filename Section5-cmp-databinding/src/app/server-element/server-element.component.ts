54213905a98c93fd03def1e93ea3ce4219df2a5c
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-10-25 16:08:24 +0530

    Lifecycle hooks

--- Section5-cmp-databinding/src/app/server-element/server-element.component.ts
+++ Section5-cmp-databinding/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,14 +6,41 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
-  
-  constructor() { }
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  constructor() {
+    console.log('constructor called.');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called')
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
-    console.log('here');
-    console.log(this.element);
+    console.log('ngOnInit called');
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
   }
 
 }
