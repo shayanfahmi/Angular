817c279672ca04d261b92028943651218925883c
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-10-29 21:03:34 +0530

    Creating directives

--- Section7-directives/src/app/app.module.ts
+++ Section7-directives/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { BasiHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasiHighlightDirective
   ],
   imports: [
     BrowserModule,
