2ca0273949b9f3b3c4012033a540f93d27a5281d
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-04 22:51:17 +0530

    Using service instead of event chain between comp

--- RecipeApp/src/app/recipes/recipe.service.ts
+++ RecipeApp/src/app/recipes/recipe.service.ts
@@ -1,7 +1,11 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
-  recipes: Recipe[] = [
+
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a test', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Country-Chuck-Roast-with-Mushroom-Gravy_EXPS_DODBZ20_132000_B07_23_3b-11.jpg?resize=522%2C522&w=680'),
     new Recipe('A Test Recipe1', 'This is a test', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Country-Chuck-Roast-with-Mushroom-Gravy_EXPS_DODBZ20_132000_B07_23_3b-11.jpg?resize=522%2C522&w=680')
   ];
