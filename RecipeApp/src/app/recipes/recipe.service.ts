61177ad7ca6346a1ca4c880cef33d352d1021391
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-05 02:37:46 +0530

    Service communication

--- RecipeApp/src/app/recipes/recipe.service.ts
+++ RecipeApp/src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
@@ -25,7 +27,13 @@ export class RecipeService {
       ])
   ];
 
+  constructor(private slService: ShoppingListService){}
+
   getRecipes() {
     return this.recipes.slice(); //to return a copy as javascript returns the reference
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.slService.addIngredients(ingredients);
   }
 }
