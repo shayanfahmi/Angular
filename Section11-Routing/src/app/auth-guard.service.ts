0d4c1f1c8339603d0ec837992954de6b2ee46b61
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-05 23:53:18 +0530

    canActivateChild

--- Section11-Routing/src/app/auth-guard.service.ts
+++ Section11-Routing/src/app/auth-guard.service.ts
@@ -1,27 +1,33 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
   constructor(
     private authService: AuthService,
-    private router: Router){}
+    private router: Router) { }
+    
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    return this.canActivate(childRoute, state);
+  }
 
   canActivate(
-    route: ActivatedRouteSnapshot, 
+    route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-      return this.authService.isAuthenticated()
-        .then(
-          (authenticated: boolean) => {
-            if(authenticated) {
-              return true;
-            }else {
-              this.router.navigate(['/']);
-            }
+    return this.authService.isAuthenticated()
+      .then(
+        (authenticated: boolean) => {
+          if (authenticated) {
+            return true;
+          } else {
+            this.router.navigate(['/']);
           }
-        );
-    }
+        }
+      );
+  }
 }
