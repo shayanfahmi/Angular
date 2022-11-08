2443721af10e34260293c399b965a82fd8f65a2a
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-08 23:15:04 +0530

    making the child component work via routing

--- RecipeApp/src/app/recipes/recipe.service.ts
+++ RecipeApp/src/app/recipes/recipe.service.ts
@@ -33,6 +33,10 @@ export class RecipeService {
     return this.recipes.slice(); //to return a copy as javascript returns the reference
   }
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]){
     this.slService.addIngredients(ingredients);
   }
