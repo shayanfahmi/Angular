b88c1fbd25df3019fb74bc616dee6bdb54421750
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-09 03:01:25 +0530

    custom observable

--- Observable/obs-01-start/src/app/home/home.component.ts
+++ Observable/obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 import { count } from 'rxjs-compat/operator/count';
 
 @Component({
@@ -16,9 +16,22 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.subs = interval(1000).subscribe(
-      count => {
-        console.log(count);
+    // this.subs = interval(1000).subscribe(
+    //   count => {
+    //     console.log(count);
+    //   }
+    // );
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.subs = customIntervalObservable.subscribe(
+      data => {
+        console.log(data);
       }
     );
   }
