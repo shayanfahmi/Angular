a65ee49f1a61901f9c153991cbd0408304b00433
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-09 15:54:16 +0530

    reset form

--- Section15-Forms/src/app/app.component.ts
+++ Section15-Forms/src/app/app.component.ts
@@ -51,5 +51,6 @@ export class AppComponent {
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
 
+    this.signupForm.reset();
   }
 }
