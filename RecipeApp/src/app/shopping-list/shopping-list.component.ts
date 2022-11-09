f8a10cd25d17609f5f22ae933edc5f9862a24254
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-09 13:01:30 +0530

    Observables and Subject

--- RecipeApp/src/app/shopping-list/shopping-list.component.ts
+++ RecipeApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,15 +8,19 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnChanges {
 
   ingredients: Ingredient[];
+  private igChangeSub: Subscription;
 
   constructor(private slService: ShoppingListService) { }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.igChangeSub.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged
+    this.igChangeSub = this.slService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
