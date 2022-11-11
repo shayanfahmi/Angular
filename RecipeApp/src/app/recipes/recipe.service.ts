e565cab62d3ac054bfc583d155e4212636a88797
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-11 09:26:42 +0530

    fetching recpe list

--- RecipeApp/src/app/recipes/recipe.service.ts
+++ RecipeApp/src/app/recipes/recipe.service.ts
@@ -6,6 +6,7 @@ import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
   private recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe', 
@@ -37,5 +38,15 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]){
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
