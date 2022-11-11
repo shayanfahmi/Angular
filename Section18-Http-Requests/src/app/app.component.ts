3bbeb85aedcd776b81ed54bdf8d1fa4171848a32
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-11 20:22:54 +0530

    map response to object

--- Section18-Http-Requests/src/app/app.component.ts
+++ Section18-Http-Requests/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +13,43 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    console.log(postData);
+    this.http.post(
+      'https://ng-recipe-app-ab758-default-rtdb.firebaseio.com/posts.json', 
+      postData).subscribe(responseData => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://ng-recipe-app-ab758-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map((responseData: {[key: string]: Post}) => {
+      const postsArray: Post[] = [];
+      for(const key in responseData){
+        if(responseData.hasOwnProperty(key)){
+          postsArray.push({...responseData[key], id: key});
+        }
+      }
+      return postsArray;
+    }))
+    .subscribe(
+      posts => {
+        console.log(posts);
+      }
+    );
   }
 }
