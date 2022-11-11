2b8f35a299ac171389088e2209c4107649e5761e
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-11 22:23:13 +0530

    catchError

--- Section18-Http-Requests/src/app/posts.service.ts
+++ Section18-Http-Requests/src/app/posts.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
-import { map } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+import { catchError, map } from "rxjs/operators";
 import { Post } from "./post.model";
 
 @Injectable({ providedIn: 'root' })
@@ -35,7 +35,11 @@ export class PostService {
         }
       }
       return postsArray;
-    }));
+    }),
+    catchError(errorRes => {
+      return throwError(errorRes);
+    })
+    );
   }
 
   deletePosts(){
