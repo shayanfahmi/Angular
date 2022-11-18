6cdb202e1f8c3fd6f5699120ad6276b58612f662
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-18 20:43:49 +0530

    Using Resolver

--- RecipeApp/src/app/shared/data-storage.service.ts
+++ RecipeApp/src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -23,17 +23,17 @@ export class DataStorageService {
       });
   }
 
-  fetchRecipes(){
-    this.http.get<Recipe[]>('https://ng-recipe-app-ab758-default-rtdb.firebaseio.com/recipes.json')
-    .pipe(map(recipes => {
-      return recipes.map(recipe => {
-        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-      });
-    }))
-    .subscribe(
-      response => {
-        this.recipeService.setRecipes(response);
-      }
-    );
+  fetchRecipes() {
+    return this.http.get<Recipe[]>('https://ng-recipe-app-ab758-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      )
   }
 }
