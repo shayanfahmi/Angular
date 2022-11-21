b80fadbf7c7367b096201c7c0e56bb0abf451f2b
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-21 17:45:04 +0530

    changes

--- RecipeApp/src/app/recipes/recipes-resolver.service.ts
+++ RecipeApp/src/app/recipes/recipes-resolver.service.ts
@@ -6,21 +6,21 @@ import { Recipe } from "./recipe.model";
 import { RecipeService } from "./recipe.service";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class RecipesResolverService implements Resolve<Recipe[]>{
-    
-    constructor(
-        private dataStorageService: DataStorageService,
-        private recipeService: RecipeService){}
-    
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-        const recipes = this.recipeService.getRecipes();
-        if(recipes.length === 0 ){
-            return this.dataStorageService.fetchRecipes();
-        }else {
-            return recipes;
-        }
+
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
+    const recipes = this.recipeService.getRecipes();
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
     }
+  }
 
 }
