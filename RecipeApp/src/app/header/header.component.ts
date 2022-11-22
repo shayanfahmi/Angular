0b0fefef135b3b5a59f7163f5637198452c6527d
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-11-22 20:11:18 +0530

    changes

--- RecipeApp/src/app/header/header.component.ts
+++ RecipeApp/src/app/header/header.component.ts
@@ -1,17 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { AuthService } from "../auth/auth.service";
 import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html'
+  selector: 'app-header',
+  templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-    constructor(private dataStorageService: DataStorageService){}
-    onSaveData(){
-        this.dataStorageService.storeRecipes();
-    }
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSub: Subscription;
+  isAuthenticated = false;
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService) { }
+  
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
+  
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(
+      user => {
+        this.isAuthenticated = !user ? false : true;
+      }
+    );
+  }
 
-    onFetchData() {
-        this.dataStorageService.fetchRecipes().subscribe();
-    }
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
 }
